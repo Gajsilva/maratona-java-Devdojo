c6e0eb30c7d4f391bfac3b2f8c6e3d116518f252
Gabriel Silva <86989966+Gajsilva@users.noreply.github.com>
2022-09-18 18:38:34 -0300

    update

--- src/academy/devdojo/Aula06EstruturaRepeticao03.java
+++ src/academy/devdojo/Aula06EstruturaRepeticao03.java
@@ -3,8 +3,8 @@ package academy.devdojo;
 public class Aula06EstruturaRepeticao03 {
     public static void main(String[] args) {
         // imprima os primeiros 25 numeros de um dado valor. Por exemplo, 50
-        int valormax = 50;
-        for (int i = 0; i <= valormax ; i++) {
+        int valormaximo = 50;
+        for (int i = 0; i <= valormaximo ; i++) {
             if (i > 25){
                 break;
             }
