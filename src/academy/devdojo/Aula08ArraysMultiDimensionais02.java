34a3e4fa4df347b18245588f3d8b59576f13649d
Gabriel Silva <86989966+Gajsilva@users.noreply.github.com>
2022-09-19 02:06:50 -0300

    update

--- src/academy/devdojo/Aula08ArraysMultiDimensionais02.java
+++ src/academy/devdojo/Aula08ArraysMultiDimensionais02.java
@@ -2,12 +2,16 @@ package academy.devdojo;
 
 public class Aula08ArraysMultiDimensionais02 {
     public static void main(String[] args) {
-        int[][] arrayint = new int[2][];
+        int[] array = {1,2,5,6,9};
+        int[][] arrayint = new int[3][];
 
-        arrayint[0] = new int[3];
-        arrayint[1] = new int[6];
+        arrayint[0] = array;
+        arrayint[1] = new int[] {1,2,3,4,5,6};
+        arrayint[2] = new int[4];
 
-        for (int[] arrayBase: arrayint) {
+        int[][] arrayInt2 = {{1,2},{0,0,3,5,6},{9,8,7,6,5,4,3,2,1}};
+
+        for (int[] arrayBase: arrayInt2) {
             System.out.println("\n-------");
             for (int num: arrayBase) {
                 System.out.print(num + " ");
