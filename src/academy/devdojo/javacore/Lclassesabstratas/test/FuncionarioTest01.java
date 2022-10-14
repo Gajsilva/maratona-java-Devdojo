0809c9fd18d65b33e54eb0c4d7c8912273e07814
gabriel <gajsilva2@gmail.com>
2022-10-14 03:13:00 -0300

    update

---
+++ src/academy/devdojo/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.javacore.Lclassesabstratas.test;
+
+import academy.devdojo.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import academy.devdojo.javacore.Lclassesabstratas.dominio.Funcionario;
+import academy.devdojo.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Law", 3500);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Touya", 6000);
+        System.out.println(gerente);
+        System.out.println(desenvolvedor);
+    }
+}
