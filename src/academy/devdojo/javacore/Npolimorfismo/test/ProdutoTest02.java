b98b7589a7f2f53ca1e0d61012accec2982f9891
gabriel <gajsilva2@gmail.com>
2022-10-15 23:58:59 -0300

    update

---
+++ src/academy/devdojo/javacore/Npolimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.javacore.Npolimorfismo.test;
+
+import academy.devdojo.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.javacore.Npolimorfismo.dominio.Tomate;
+
+public class ProdutoTest02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Rizen", 5000);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+        System.out.println("----------------------------------");
+
+        Produto produto1 = new Tomate("Tomatinho", 30);
+        System.out.println(produto1.getNome());
+        System.out.println(produto1.getValor());
+        System.out.println(produto1.calcularImposto());
+        System.out.println("----------------------------------");
+    }
+}
