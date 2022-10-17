11115970664e84d5c4325565ae6755689e81f551
gabriel <gajsilva2@gmail.com>
2022-10-16 23:06:39 -0300

    update

---
+++ src/academy/devdojo/javacore/Oexecetion/RuntimeFinally/test/RunTimeExceptionTest03.java
@@ -0,0 +1,32 @@
+package academy.devdojo.javacore.Oexecetion.RuntimeFinally.test;
+
+public class RunTimeExceptionTest03 {
+    public static void main(String[] args) {
+        abreConexao();
+        System.out.println("-----------------");
+        abreConexao2();
+    }
+
+    private static String abreConexao() {
+        try {
+            System.out.println("Abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+            return "conexão aberta";
+        }catch (Exception e){
+            System.out.println("");
+            e.printStackTrace();
+        }finally {
+            System.out.println("Fechando arquivo liberado pelo sistema operacional");
+        }
+        return null;
+    }
+
+    private static void abreConexao2(){
+        try {
+            System.out.println("abrindo arquivo");
+            System.out.println("Escrevendo dados no arquivo");
+        }finally {
+            System.out.println("Fechando recurso liberado pelo SO");
+        }
+    }
+}
