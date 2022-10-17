ebee24292f98e2b2cc861bd37b77449b8ff29c17
gabriel <gajsilva2@gmail.com>
2022-10-16 21:08:56 -0300

    update

---
+++ src/academy/devdojo/javacore/Oexecetion/runtime/test/RunTimeExeptionTest02.java
@@ -0,0 +1,27 @@
+package academy.devdojo.javacore.Oexecetion.runtime.test;
+
+public class RunTimeExeptionTest02 {
+    public static void main(String[] args) {
+        try {
+            divisao(1,0);
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+        System.out.println("Codigo FInalizado");
+    }
+
+    /**
+     *
+     * @param a
+     * @param b não pode ser zerp
+     * @return
+     * @throws IllegalArgumentException caso b seja zero
+     */
+    private static int divisao(int a, int b){
+        if (b == 0){
+            throw new IllegalArgumentException("Argumento ilegal, nao pode ser 0");
+        }
+        return a/b;
+    }
+
+}
