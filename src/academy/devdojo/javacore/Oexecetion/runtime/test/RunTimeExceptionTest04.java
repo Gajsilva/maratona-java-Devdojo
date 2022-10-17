98fe6d69ce22545c341ff4e5ccbd99bdf37dc78a
gabriel <gajsilva2@gmail.com>
2022-10-17 00:34:32 -0300

    update

--- src/academy/devdojo/javacore/Oexecetion/runtime/test/RunTimeExceptionTest04.java
+++ src/academy/devdojo/javacore/Oexecetion/runtime/test/RunTimeExceptionTest04.java
@@ -7,26 +7,19 @@ import java.sql.SQLException;
 public class RunTimeExceptionTest04 {
     public static void main(String[] args) {
         try{
-            throw new ArrayIndexOutOfBoundsException();
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Dentro da ArrayIndexOutOfBoundsException");
-        }catch (IndexOutOfBoundsException e){
-            System.out.println("Dentro do IndexOutOfBoundsException");
-        }catch (IllegalArgumentException e){
-            System.out.println("Dentro do IllegalArgumentException");
-        }catch (ArithmeticException e){
-            System.out.println("Dentro do ArithmeticException");
+            throw new RuntimeException();
+        }catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e){
+            System.out.println("ArrayIndexOutOfBoundsException | IndexOutOfBoundsException | IllegalArgumentException");
         }catch (RuntimeException e){
             System.out.println("Dentro do RuntimeException");
         }
 
         try{
             talvezLanceExcetion();
-        }catch (SQLException throwables){
-            throwables.printStackTrace();
-        }catch (FileNotFoundException e){
+        }catch (IOException | SQLException e){
             e.printStackTrace();
         }
+
     }
 
     private static void talvezLanceExcetion() throws SQLException, FileNotFoundException{
