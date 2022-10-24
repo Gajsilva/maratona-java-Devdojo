242d94dfb9a8b05f232bfd7ae1f510a872ca289f
gabriel <gajsilva2@gmail.com>
2022-10-23 21:09:15 -0300

    update

--- src/academy/devdojo/javacore/Wnio/test/PathTest02.java
+++ src/academy/devdojo/javacore/Wnio/test/PathTest02.java
@@ -1,7 +1,27 @@
 package academy.devdojo.javacore.Wnio.test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class PathTest02 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Path pastaPath = Paths.get("pasta");
+        if (Files.notExists(pastaPath)) {
+            Path pastaDirectories = Files.createDirectory(pastaPath);
+        }
+        Path subPastaPh = Paths.get("pasta/subpasta/subsubpasta");
+        Path subPastaDirectory = Files.createDirectories(subPastaPh);
+        Path filePath = Paths.get(subPastaPh.toString(),"file.txt");
+        if (Files.notExists(filePath)) {
+            Path filePathCreate = Files.createFile(filePath);
+        }
+
+        Path source = filePath;
+        Path target = Paths.get(filePath.getParent().toString(),"file_renamed.txt");
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
 
     }
 }
