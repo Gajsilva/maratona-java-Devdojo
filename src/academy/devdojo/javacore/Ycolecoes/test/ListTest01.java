f51a8ac2c7d8132a750bcd2a50c1132798d5db64
gabriel <gajsilva@gmail.com>
2022-10-30 01:17:49 -0300

    update

--- src/academy/devdojo/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/javacore/Ycolecoes/test/ListTest01.java
@@ -6,10 +6,17 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(16);
+        List<String> nomes1 = new ArrayList<>(16);
         nomes.add("Gabriel");
         nomes.add("DevDojo");
         nomes.add("Willian");
         nomes.add("Java");
+        nomes1.add("Script");
+        nomes1.add("Academy");
+        nomes1.add("SQL");
+        nomes.remove("Java");
+
+        nomes.addAll(nomes1);
 
         for (String nome : nomes){
             System.out.println(nome);
@@ -19,5 +26,6 @@ public class ListTest01 {
         for (int i = 0;i < nomes.size(); i++){
             System.out.println(nomes.get(i));
         }
+
     }
 }
