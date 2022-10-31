590a731e067703d81d355af0c8096995c6a9dec8
gabriel <gajsilva@gmail.com>
2022-10-30 21:51:11 -0300

    update

--- src/academy/devdojo/javacore/Ycolecoes/test/BinarySearchTest01.java
+++ src/academy/devdojo/javacore/Ycolecoes/test/BinarySearchTest01.java
@@ -4,39 +4,38 @@ import academy.devdojo.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
-class MangaByIdComarator implements Comparator<Manga>{
-
-    @Override
-    public int compare(Manga o1, Manga o2) {
-        //return o1.getId().compareTo(o2).getId();
-        return 0;
-    }
-}
-
-public class MangaSortTest01 {
+public class BinarySearchTest01 {
     public static void main(String[] args) {
+        List<Integer>  numeros = new ArrayList<>();
+        numeros.add(2);
+        numeros.add(0);
+        numeros.add(6);
+        numeros.add(3);
+        //(-(ponto de inserção)-1)
+        Collections.sort(numeros);
+        System.out.println(Collections.binarySearch(numeros,3));
+
         List<Manga> mangas = new ArrayList<>(6);
+        MangaByIdComarator mangaByIdComarator = new MangaByIdComarator();
         mangas.add(new Manga(5L,"Attack on titan",20.9));
         mangas.add(new Manga(6L,"Sword art online Kirito",29.9));
         mangas.add(new Manga(2L,"Bersek",15.90));
         mangas.add(new Manga(3L,"Pokemon Arc",35.9));
         mangas.add(new Manga(4L,"Bleach 2022",39.9));
-        for (Manga manga : mangas){
-            System.out.println(manga);
-        }
-        Collections.sort(mangas);
-        System.out.println("-------------------------");
+
+       // Collections.sort(mangas);
+
+        mangas.sort(new MangaByIdComarator());
+
         for (Manga manga : mangas){
             System.out.println(manga);
         }
 
-        Collections.sort(mangas, new MangaByIdComarator());
-        System.out.println("-------------------------");
-        for (Manga manga : mangas){
-            System.out.println(manga);
-        }
+        Manga mangaTosearch = new Manga(2L,"Bersek", 15.90);
+
+        System.out.println(Collections.binarySearch(mangas,mangaTosearch, mangaByIdComarator));
+
     }
 }
