1d3b4969d1853f0deb2458695eea8a75ccfb781f
gabriel <gajsilva@gmail.com>
2022-11-07 21:25:24 -0300

    update

---
+++ src/academy/devdojo/javacore/ZZEstreams/test/StreamTest03.java
@@ -0,0 +1,25 @@
+package academy.devdojo.javacore.ZZEstreams.test;
+
+import academy.devdojo.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTest03 {
+    private static List<LightNovel> lightNovels = new ArrayList<>( List.of(
+            new LightNovel("Tensei Shitara",1.50),
+            new LightNovel("Overlord",1.59),
+            new LightNovel("Fullmetal Alchemist",4.99),
+            new LightNovel("Blue Lock", 2.99),
+            new LightNovel("Aoshi", 3.50),
+            new LightNovel("High School of the dead", 3.90)
+    ));
+
+    public static void main(String[] args) {
+        Stream<LightNovel> stream = lightNovels.stream();
+        lightNovels.stream().forEach(System.out::println);
+        long count = stream.filter(ln -> ln.getPrice() <= 4).count();
+        System.out.println(count);
+    }
+}
