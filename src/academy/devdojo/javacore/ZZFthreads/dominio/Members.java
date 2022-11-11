e0997e7be78a4c445cc5ed6fb5d9fc1134edf47e
gabriel <gajsilva@gmail.com>
2022-11-10 23:37:55 -0300

    update

---
+++ src/academy/devdojo/javacore/ZZFthreads/dominio/Members.java
@@ -0,0 +1,43 @@
+package academy.devdojo.javacore.ZZFthreads.dominio;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Members {
+    private final Queue<String> emails = new ArrayBlockingQueue<>(10);
+    private boolean open = true;
+
+    public boolean isOpen(){
+        return open;
+    }
+    public int pendingEmails(){
+        synchronized (emails){
+            return emails.size();
+        }
+    }
+
+    public void addMemberEmail(String email){
+        synchronized (this.emails){
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " Adicionou email na lista");
+            this.emails.add(email);
+            this.emails.notifyAll();
+        }
+    }
+
+    public String retrieveEmail() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName()+ " checking if thread are emails");
+        synchronized (this.emails){
+            while (this.emails.size() == 0) {
+                if (!open) break;
+                System.out.println(Thread.currentThread().getName()+ " Não tem email disponivel na lista, entrando em modo de espera");
+                this.emails.wait();
+            }
+            return this.emails.poll();
+        }
+    }
+
+    public void close(){
+        System.out.println(Thread.currentThread().getName() + " ");
+    }
+}
