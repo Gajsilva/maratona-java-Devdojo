eb403855ede26350a1bb418ea7c7b013669b9424
gabriel <gajsilva@gmail.com>
2022-11-26 04:59:38 -0300

    update

---
+++ src/academy/devdojo/javacore/ZZHpadroesprojeto/test/BuildPatternTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.javacore.ZZHpadroesprojeto.test;
+
+import academy.devdojo.javacore.ZZHpadroesprojeto.dominio.Person;
+
+public class BuildPatternTest01 {
+    public static void main(String[] args) {
+        Person build =new Person.PersonBuilder()
+                .firstName("Gabriel")
+                .lastName("silva")
+                .username("viradojiraya")
+                .email("jiraya@gmail.com")
+                .build()
+        ;
+
+        System.out.println(build);
+    }
+}
