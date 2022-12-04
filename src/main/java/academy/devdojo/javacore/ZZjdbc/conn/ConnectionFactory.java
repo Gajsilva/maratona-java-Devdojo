bbf36e2ddcd7dd751a467d690191850b035b782b
gabriel <gajsilva@gmail.com>
2022-12-04 11:09:22 -0300

    update

--- src/main/java/academy/devdojo/javacore/ZZjdbc/conn/ConnectionFactory.java
+++ src/main/java/academy/devdojo/javacore/ZZjdbc/conn/ConnectionFactory.java
@@ -1,8 +1,15 @@
 package academy.devdojo.javacore.ZZjdbc.conn;
 
+import com.sun.jdi.connect.spi.Connection;
+
 public class ConnectionFactory {
     //java.sql = Connection, Statement , ResultSet, DriverManager
-    public static void main(String[] args) {
 
+    public Connection getConnection(){
+        String url;
+        String username;
+        String password;
+        return null;
     }
+
 }
