667ec4ee1adb10da91fca91c36f5ea322d8fc85c
gabriel <gajsilva@gmail.com>
2022-12-04 08:25:25 -0300

    update

---
+++ src/main/java/academy/devdojo/javacore/ZZjdbc/conn/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package academy.devdojo.javacore.ZZjdbc.conn;
+
+public class ConnectionFactory {
+    //java.sql = Connection, Statement , ResultSet, DriverManager
+    public static void main(String[] args) {
+
+    }
+}
